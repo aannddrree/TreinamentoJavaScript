f1a45a80f131dc7f0604181125a022fd22ce6235
unknown <aannddrree>
2021-03-16 14:28:52 -0300

    cep

---
+++ cep.js
@@ -0,0 +1,47 @@
+const url = 'https://viacep.com.br/ws/$cep/json/';
+
+function findCEP(){
+    var cep = document.getElementById("cep").value;
+    var urlWithCEP = url.replaceAll("$cep", cep).replaceAll("-","");
+    loadDoc(urlWithCEP);
+}
+
+function loadDoc(urlWithCEP){
+    this.getJSON(urlWithCEP, function(err, data){
+
+        /*console.log("logradouro: " + data.logradouro);
+        console.log("complemento: " + data.complemento);
+        console.log("bairro: " + data.bairro);
+        console.log("localidade: " + data.localidade);
+        console.log("uf: " + data.uf);
+        console.log("ibge: " + data.ibge);
+        console.log("gia: " + data.gia);
+        console.log("ddd: " + data.ddd);
+        console.log("siafi: " + data.siafi);*/
+
+        document.getElementById("logradouro").value = data.logradouro;
+        document.getElementById("bairro").value = data.bairro;
+        document.getElementById("cidade").value = data.localidade;
+        document.getElementById("uf").value = data.uf;
+    });
+}
+
+var getJSON = function(url, callback){
+
+    var  xhr  = new XMLHttpRequest(); //XMLHttpRequest é um objeto que 
+                                      //fornece funcionalidade ao cliente para 
+                                      //transferir dados entre um cliente e um servidor.
+
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function(){
+        var status = xhr.status;
+        if (status === 200){
+            callback(null,  xhr.response);
+        }else{
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+}
+
